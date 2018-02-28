2eb8fcc406b37d7bccded7357f701daafd71ca99
Ievgen Safronenko <ievgen.safronenko@gmail.com>
2018-02-28 16:42:21 +0100

    Added pagination to previous conversions

--- src/main/java/com/ievgensafronenko/currencyconverter/ratesintegration/repository/PreviousConversionsRepository.java
+++ src/main/java/com/ievgensafronenko/currencyconverter/ratesintegration/repository/PreviousConversionsRepository.java
@@ -1,6 +1,7 @@
 package com.ievgensafronenko.currencyconverter.ratesintegration.repository;
 
 import com.ievgensafronenko.currencyconverter.ratesintegration.model.PreviousConversions;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,5 @@ import java.util.List;
  * Repository for persisting historical data.
  */
 public interface PreviousConversionsRepository extends JpaRepository<PreviousConversions, Long> {
-    List<PreviousConversions> findAllByUserEmailOrderByDateDesc(String email);
+    List<PreviousConversions> findAllByUserEmailOrderByDateDesc(String email, Pageable pageable);
 }
--- src/main/java/com/ievgensafronenko/currencyconverter/ratesintegration/service/PreviousConversionsStorageService.java
+++ src/main/java/com/ievgensafronenko/currencyconverter/ratesintegration/service/PreviousConversionsStorageService.java
@@ -3,6 +3,9 @@ package com.ievgensafronenko.currencyconverter.ratesintegration.service;
 import com.ievgensafronenko.currencyconverter.ratesintegration.model.PreviousConversions;
 import com.ievgensafronenko.currencyconverter.ratesintegration.repository.PreviousConversionsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,11 +19,15 @@ public class PreviousConversionsStorageService {
     @Autowired
     PreviousConversionsRepository repository;
 
+    @Value("${previous.results.count}")
+    private Integer size;
+
     public void save(PreviousConversions data) {
         repository.save(data);
     }
 
     public List<PreviousConversions> findByUserEmailOrderByDateDesc(String userEmail) {
-        return repository.findAllByUserEmailOrderByDateDesc(userEmail);
+        Pageable tenResults = new PageRequest(0, size);
+        return repository.findAllByUserEmailOrderByDateDesc(userEmail, tenResults);
     }
 }
